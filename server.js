78e66b13f796ce66ad30ee3fb4741ec1f9cde44a
jmatos01 <jmatos@techlaunch.io>
2018-08-22 19:33:32 -0400

    first commit

--- server.js
+++ server.js
@@ -50,6 +50,27 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "form.html"));
 }); // listensnt to any get request
 
+//purpose of this is to enable cross domain requests
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://142.93.206.136:4000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.post("/", function(req, res) {
   //listens to any post request
   console.log(req.body);
